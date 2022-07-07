69b340d7411d490162fa1247d0835ecc82a8e829
powcoder代写 <powcoder@163.com>
2022-07-07 14:29:02 +0800

    程序代写 加微信 powcoder

---
+++ tests/test_kb2.pl
@@ -0,0 +1,28 @@
+mother(anne, peter) .
+mother(anne, zara) .
+mother(sarah, beatrice) .
+mother(sarah, eugenie) .
+mother(elizabeth, anne) .
+mother(elizabeth, andrew) .
+
+father(mark, peter) .
+father(mark, zara) .
+father(andrew, beatrice) .
+father(andrew, eugenie) .
+father(philip, anne) .
+father(philip, andrew) .
+
+male(philip) .
+male(mark) .
+male(andrew) .
+male(peter) .
+
+female(elizabeth) .
+female(anne) .
+female(sarah) .
+female(zara) .
+female(beatrice) .
+female(eugenie) .
+
+parent(X,Y) :- mother(X,Y) .
+parent(X,Y) :- father(X,Y) .
